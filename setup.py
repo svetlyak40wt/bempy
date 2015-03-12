8f4105ebb315ae95eba8d2478b800c27945d3f4a
Alexander Artemenko <svetlyak.40wt@gmail.com>
2015-03-12 16:58:36 +0300

    Add clint as requirement.

--- setup.py
+++ setup.py
@@ -2,4 +2,6 @@ from setuptools import setup, find_packages
 
 setup(name='bempy',
       version='0.1.0',
-      packages=find_packages())
+      packages=find_packages(),
+      install_requires=[
+          'clint'])
