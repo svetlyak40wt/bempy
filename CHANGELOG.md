ad87982d17c2d14c344d9e3d91a48c37dfb72535
Alexander Artemenko <svetlyak.40wt@gmail.com>
2015-03-19 15:08:32 +0300

    Version 0.1.1

---
+++ CHANGELOG.md
@@ -0,0 +1,4 @@
+0.1.1 (2015-03-19)
+==================
+
+* Make wrapper returned by `returns_blocks` decorator, understand additional `args` and `kwargs` arguments.
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='bempy',
-      version='0.1.0',
+      version='0.1.1',
       packages=find_packages(),
       install_requires=[
           'clint'])
